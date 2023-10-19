d6be756a2bcab20aadee4bd13981ca86d4e7413b
smithaagarwal <smitha.kotian@gmail.com>
2023-10-19 13:54:14 +0100

    feat: write logic for first 2 tasks

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -17,15 +17,25 @@ people.forEach(logPerson);
 // Store the logPerson() function in a variable called performLogPerson
 
 // Your code here
+const funcInVarLogPerson = function(person) {
+  console.log("The person is " + person);
+}
+
+people.forEach(funcInVarLogPerson);
 
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
 
 // Your code here
+const ArrowFuncInVarLogPerson = (person) => {
+    console.log("The person is " + person);
+  }
 
+  people.forEach(ArrowFuncInVarLogPerson);
+  
 // Anonymous Arrow Functions
 // Replace logPerson below with an Anonymous Arrow Function that does the same thing!
 
-people.forEach(logPerson);
+people.forEach( person => console.log("The person is " + person));
 
 // In JavaScript there are lots of ways to write functions. There is no right or wrong way, but arrow functions are the most modern. 🙂
--- modern_javascript_exercises/variables/const_vs_let.js
+++ modern_javascript_exercises/variables/const_vs_let.js
@@ -1,7 +1,7 @@
 // Instructions can be found in const_vs_let.md
 
 function countThePeople() {
-  const count = 0;
+  let count = 0;
   let people = ["Barbara", "Jacob", "Angela", "Brangela"];
   people.forEach(() => (count += 1));
   return count;
