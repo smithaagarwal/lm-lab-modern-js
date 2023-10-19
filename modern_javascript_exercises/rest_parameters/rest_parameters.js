49ef0624cfe53155803fd0392b9439c94357a8c6
Smitha <smitha.kotian@gmail.com>
2023-10-19 16:31:52 +0100

    feat: add logic to use rest parameters

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,7 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...numbers) {
+  return numbers.reduce((total, number)=> total + number,0);
 }
 
-add(1, 2, 3, 4, 5);
