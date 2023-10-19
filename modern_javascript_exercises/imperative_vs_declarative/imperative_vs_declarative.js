182cff990b0670ec111a61f8aa0196d95eb09c90
Smitha <smitha.kotian@gmail.com>
2023-10-19 16:17:55 +0100

    feat: add logic to use declarative style

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -2,12 +2,5 @@
 
 export function longPasswords(passwords) {
   let longPasswords = [];
-
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
-    if (password.length >= 9) {
-      longPasswords.push(password);
-    }
-  }
-  return longPasswords;
+  return passwords.filter(password => (password.length >= 9));
 }
